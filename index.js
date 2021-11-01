f12856eb0a8aed9a985e2b7ddb384176077404e4
Divyansh Tamrakar <tamrakardivyansh189@gmail.com>
2021-11-01 19:42:39 +0530

    showed output

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var button = document.querySelector(".but");
 let input = document.querySelector("#text-input");
-
+let output = document.querySelector(".output")
 
 
 
 button.addEventListener("click", Callbackfunction);
 
 
-
 function Callbackfunction() {
   console.log("click");
   console.log(input.value)
+  output.innerText = input.value
 }
